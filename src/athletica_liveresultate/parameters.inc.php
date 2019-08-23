8c11e2ff7e8bd8dfb241a323c42cb590af89cd52
Andi Meier <andi@daemeier-clan.ch>
2019-08-23 09:36:43 +0200

    src: adapt to docker's db server

--- src/athletica_liveresultate/parameters.inc.php
+++ src/athletica_liveresultate/parameters.inc.php
@@ -9,7 +9,7 @@
  */
  
 // Athletica Server mit Wettkampf-Daten
-$cfgDBhost_live = '127.0.0.1'; // Athletica Server 
+$cfgDBhost_live = 'db'; // Athletica Server
 $cfgDBport_live = '';          // MySQL port - leave blank for default port
 $cfgDBname_live =  'athletica';   // database
 $cfgDBuser_live = 'athletica';   // user
@@ -17,7 +17,7 @@ $cfgDBpass_live = 'athletica';   // password
 
 
 // Datenbank mit Installation der Live Resultate 
-$cfgDBhost = '127.0.0.1'; // MySQL hostname      
+$cfgDBhost = 'db'; // MySQL hostname
 $cfgDBport = '';          // MySQL port - leave blank for default port
 $cfgDBname = 'athletica_liveResultate';   // database
 $cfgDBuser = 'athletica';   // user
